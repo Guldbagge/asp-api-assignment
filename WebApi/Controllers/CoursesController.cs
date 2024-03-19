fb93270c8e6351b99d07d13618f7d4ed3d3dc533
Edvin <edvin.thornholm@gmail.com>
2024-03-19 14:33:05 +0100

    subscribe

---
+++ WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController(DataContext context) : ControllerBase
+    {
+        private readonly DataContext _context = context;
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll() => Ok(await _context.Courses.ToListAsync());
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne(int id)
+        {
+            var course = await _context.Courses.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (course != null)
+            {
+                return Ok(course);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOne(CourseEntity model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Courses.Add(model);
+                    await _context.SaveChangesAsync();
+
+                    return Created("", model);
+                }
+
+                catch
+                {
+                    return Problem("Unable to create course.");
+                }
+            }
+
+            return BadRequest();
+        }
+
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Web.Helpers;
+
+namespace WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    { 
+        using var http = new HttpClient();
+        var response = await http.GetAsync("https://localhost:7026/api/courses");
+        var json = await response.Content.ReadAsStringAsync();
+        var data = JsonConvert.DeserializeObject<IEnumerable<CourseEntity>>(json);
+   
+        return View(data);
+    }
+
+    [Route("/Details")]
+    public async Task<IActionResult> Details()
+    {
+        using var http = new HttpClient();
+        var response = await http.GetAsync($"https://localhost:7026/api/courses/1");
+        var json = await response.Content.ReadAsStringAsync();
+        var data = JsonConvert.DeserializeObject<CourseEntity>(json);
+        return View(data);
+    }
+
+
+    [Route("/Subscribe")]
+    public IActionResult Subscribe()
+    {
+        ViewData["Subscribe"]=false;
+        return View();
+    }
+
+    [Route("/Subscribe")]
+    [HttpPost]
+    public async Task<IActionResult> Subscribe(SubscriberEntity model)
+    {
+        if (ModelState.IsValid)
+        {
+            using var http = new HttpClient();
+
+            var json = JsonConvert.SerializeObject(model);
+            using var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await http.PostAsync($"https://localhost:7026/api/subscribers?email={model.Email}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                ViewData["Subscribe"]=true;
+            }
+
+        }
+
+        return View();
+    }
+
+}
